0ca0e4dd0d0f6332bca8786329dc198cf3c2d81d
mialana <aliu@amyliu.dev>
2026-06-24 00:24:05 -0700

    Increase movement speed

--- TheBeachEpisode/Source/TheBeachEpisode/TheBeachEpisodeCharacter.cpp
+++ TheBeachEpisode/Source/TheBeachEpisode/TheBeachEpisodeCharacter.cpp
@@ -30,7 +30,7 @@ ATheBeachEpisodeCharacter::ATheBeachEpisodeCharacter()
     // instead of recompiling to adjust them
     GetCharacterMovement()->JumpZVelocity = 500.f;
     GetCharacterMovement()->AirControl = 0.35f;
-    GetCharacterMovement()->MaxWalkSpeed = 500.f;
+    GetCharacterMovement()->MaxWalkSpeed = 800.f;
     GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
     GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
     GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
